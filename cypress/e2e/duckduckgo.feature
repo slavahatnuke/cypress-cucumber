429c79bae0955cef390f98c989a84a63d8991ad8
slava <slava.hatnuke@gmail.com>
2023-08-31 22:52:11 +0100

    Good one

--- cypress/e2e/duckduckgo.feature
+++ cypress/e2e/duckduckgo.feature
@@ -1,5 +1,8 @@
 Feature: duckduckgo.com
 
+  Background:
+    Given Duck Duck go world
+
   Scenario: X1
     When I visit duckduckgo.com
     Then I should see a search bar
--- cypress/e2e/duckduckgo.ts
+++ cypress/e2e/duckduckgo.ts
@@ -1,21 +1,60 @@
-import {When, Then} from "@badeball/cypress-cucumber-preprocessor";
-import {IDuckDuckGoWorld} from "./duckduckgo-world";
-import expect from 'expect';
+import {Then, When, Before, Given} from "@badeball/cypress-cucumber-preprocessor";
 
-When("I visit duckduckgo.com", function (this: IDuckDuckGoWorld) {
-  cy.visit("https://duckduckgo.com/").then(async () => {
-    this.hello = true;
+export type IBehaviouralWorld<Type extends {}> = Type & Mocha.Context;
+export type IDuckDuckGoWorld = IBehaviouralWorld<{
+  duckDuckGo: {
+    visited: boolean;
+    url: string
+  };
+}>;
+
+Before(function (this: IDuckDuckGoWorld) {
+  this.duckDuckGo = {
+    url: "https://duckduckgo.com/",
+    visited: false
+  }
+
+  cy.wrap(this.duckDuckGo).as('duckDuckGo')
+})
+
+
+Given(/^Duck Duck go world$/, function (this: IDuckDuckGoWorld) {
+  // some things to setup the world
+  this.duckDuckGo.url = "https://duckduckgo.com/";
+
+  cy.get('@duckDuckGo').then(async (duckDuckGo) => {
+
+    cy.request({
+      method: 'GET',
+      // url to take somethign public from github
+      url: 'https://api.github.com/repos/cypress-io/cypress/commits?per_page=1&sha=master',
+    }).then(async (response) => {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 300)
+      })
+      cy.log('made some async action', duckDuckGo)
+      cy.log('response status', response.status)
+    })
+
+
   });
 });
 
-Then("I should see a search bar", () => {
+
+When("I visit duckduckgo.com", function (this: IDuckDuckGoWorld) {
+  cy.visit(this.duckDuckGo.url).then(async () => {
+    this.duckDuckGo.visited = true;
+  });
+});
+
+Then("I should see a search bar", function (this: IDuckDuckGoWorld) {
   // cy.get('"input[type=text]"').invoke('val').then((value) => {
   //   cy.log('value', value)
   //   expect(value).toEqual('john.doe');
   // });
 
   cy.get("input[type=text]").then(async (element) => {
-    expect(true).toEqual(true);
+    // expect(true).toEqual(true);
     assert.equal(element.val(), '');
     assert.equal(element.attr('placeholder'), "Search the web without being tracked");
   })
@@ -24,6 +63,9 @@ Then("I should see a search bar", () => {
     "placeholder",
     "Search the web without being tracked"
   );
-  //
+
+  console.log('>>> duck duck go world', this.duckDuckGo)
+  cy.log('duck duck go world', this.duckDuckGo)
+
   assert.deepEqual({}, {});
 });
